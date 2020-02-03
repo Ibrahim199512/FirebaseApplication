7afd42b1b6a711da3f426376a8090c8616c67926
ibrahim alwadiya <ibrahimalwadiya@gmail.com>
2020-02-03 16:01:50 +0200

    Authentication Done

---
+++ FirebaseApplication/app/src/main/java/test/firebase/application/FirebaseUISigninActivity.java
@@ -0,0 +1,112 @@
+package test.firebase.application;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FirebaseUISigninActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_uisignin);
+        createSignInIntent();
+    }
+
+
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+//                new AuthUI.IdpConfig.TwitterBuilder().build()
+        );
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                updateUI(user);
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                updateUI(null);
+            }
+        }
+    }
+    // [END auth_fui_result]
+
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            createSuccessSnackbar(findViewById(R.id.layout), "The Name is: " + user.getDisplayName()).show();
+        } else {
+            createFailSnackbar(findViewById(R.id.layout), "you have logged out please login again").show();
+        }
+    }
+
+    public Snackbar createSuccessSnackbar(@NonNull View view, @NonNull String message) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        snackbar.getView().setBackgroundColor(view.getContext().getResources().getColor(R.color.colorPrimary));
+
+        return snackbar;
+    }
+
+    public Snackbar createFailSnackbar(@NonNull View view, @NonNull String message) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        snackbar.getView().setBackgroundColor(view.getContext().getResources().getColor(R.color.fail_color));
+
+        return snackbar;
+    }
+
+}
